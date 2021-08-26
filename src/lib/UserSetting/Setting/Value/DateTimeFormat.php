0b6ba5c388ad5db8edd45863cf63a73748def8f2
Łukasz Ostafin <ostafin.lucas@gmail.com>
2021-08-26 10:50:14 +0200

    IBX-920: Redesign pagination (#97)

--- src/lib/UserSetting/Setting/Value/DateTimeFormat.php
+++ src/lib/UserSetting/Setting/Value/DateTimeFormat.php
@@ -10,7 +10,7 @@ namespace Ibexa\User\UserSetting\Setting\Value;
 
 final class DateTimeFormat
 {
-    /** @var string |null */
+    /** @var string|null */
     private $dateFormat;
 
     /** @var string|null */
