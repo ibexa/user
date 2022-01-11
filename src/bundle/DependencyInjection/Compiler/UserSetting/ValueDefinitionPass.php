c80589a2d9657ddfcf8bb5faa6c55c9987ad8f15
Andrew Longosz <alongosz@users.noreply.github.com>
2022-01-11 14:56:59 +0100

    IBX-1694: Rebranded dependency injection container service tags

--- src/bundle/DependencyInjection/Compiler/UserSetting/ValueDefinitionPass.php
+++ src/bundle/DependencyInjection/Compiler/UserSetting/ValueDefinitionPass.php
@@ -19,7 +19,7 @@ class ValueDefinitionPass implements CompilerPassInterface
 {
     use PriorityTaggedServiceTrait;
 
-    public const TAG_NAME = 'ezplatform.admin_ui.user_setting.value';
+    public const TAG_NAME = 'ibexa.user.setting.value';
     public const GROUP_TAG_NAME = 'ibexa.user.setting.group';
 
     /**
