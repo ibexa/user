8c178f42054fef3f37aa558d079752dc4a84491c
Andrew Longosz <alongosz@users.noreply.github.com>
2024-03-08 22:18:14 +0100

    Introduced Value Object strict getters and deprecated magic ones

--- src/lib/UserSetting/UserSetting.php
+++ src/lib/UserSetting/UserSetting.php
@@ -11,16 +11,15 @@ namespace Ibexa\User\UserSetting;
 use Ibexa\Contracts\Core\Repository\Values\ValueObject;
 
 /**
- * @property string $identifier
- * @property string $name
- * @property string $description
- * @property string $value
+ * @property string $identifier @deprecated use {@see UserSetting::getIdentifier()} instead.
+ * @property string $name @deprecated use {@see UserSetting::getName()} instead.
+ * @property string $description @deprecated use {@see UserSetting::getDescription()} instead.
+ * @property string $value @deprecated use {@see UserSetting::getValue()} instead.
  */
 class UserSetting extends ValueObject
 {
     /** @var string */
     protected $identifier;
-
     /** @var string */
     protected $name;
 
@@ -29,6 +28,26 @@ class UserSetting extends ValueObject
 
     /** @var string */
     protected $value;
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
 }
 
 class_alias(UserSetting::class, 'EzSystems\EzPlatformUser\UserSetting\UserSetting');
