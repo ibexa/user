cf2acf3c5ee6e5d2e5ccd1d08cf791965d56ace5
Dawid Parafinski <dawid.parafinski@ibexa.co>
2025-06-24 12:46:20 +0200

    IBX-10170: Make access check work with all types of controller references

--- src/lib/EventListener/PerformAccessCheckSubscriber.php
+++ src/lib/EventListener/PerformAccessCheckSubscriber.php
@@ -8,17 +8,43 @@ declare(strict_types=1);
 
 namespace Ibexa\User\EventListener;
 
-use Ibexa\Contracts\User\Controller\AccessCheckController;
+use Ibexa\Contracts\User\Controller\RestrictedControllerInterface;
+use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
 
 final class PerformAccessCheckSubscriber implements EventSubscriberInterface
 {
+    public function __construct(
+        #[AutowireIterator('controller.service_arguments')]
+        private readonly iterable $controllers
+    ) {
+    }
+
     public function onControllerArgumentsEvent(ControllerArgumentsEvent $event): void
     {
         $controller = $event->getController();
-        if (is_array($controller) && $controller[0] instanceof AccessCheckController) {
+        if (is_array($controller) && $controller[0] instanceof RestrictedControllerInterface) {
             $controller[0]->performAccessCheck();
+
+            return;
+        }
+
+        if ($controller instanceof RestrictedControllerInterface) {
+            $controller->performAccessCheck();
+
+            return;
+        }
+
+        if (is_string($controller) && str_contains($controller, '::')) {
+            [$class] = explode('::', $controller, 2);
+
+            foreach ($this->controllers as $controllerInstance) {
+                if ($controllerInstance::class === $class && $controllerInstance instanceof RestrictedControllerInterface) {
+                    $controllerInstance->performAccessCheck();
+                    break;
+                }
+            }
         }
     }
 
