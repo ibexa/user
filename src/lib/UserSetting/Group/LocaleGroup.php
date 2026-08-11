77c0e50b5ebcaec0b91189cd06b2ee786c48998d
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2026-08-11 09:42:51 +0200

    IBX-11774: Changed location into locale in user settings group configuration (#135)

--- src/lib/UserSetting/Group/LocaleGroup.php
+++ src/lib/UserSetting/Group/LocaleGroup.php
@@ -11,11 +11,7 @@ namespace Ibexa\User\UserSetting\Group;
 use JMS\TranslationBundle\Annotation\Desc;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-/**
- * The translation for the given group was changed to 'Locale' from 'Location'. However, for backwards compatibility
- * we decided to keep the old configuration with corresponding classes and services as 'location'.
- */
-final class LocationGroup extends AbstractGroup
+final class LocaleGroup extends AbstractGroup
 {
     /**
      * @param array<string, \Ibexa\Contracts\User\UserSetting\ValueDefinitionInterface> $values
